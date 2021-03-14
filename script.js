36efceaf8d8a50cacf49f9ae6a15ca0d1bf65103
Rodrigo Galicia <rodrigo_galicia95@yahoo.com>
2021-03-14 14:24:07 -0500

    did something new

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
-  var length = 8,;
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-      retVal = "";
+  var lengthMessage = prompt("Enter desired length of password (between 8 - 128 characters): ");
+  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()",
+  retVal = "";
   for (var i = 0, n = charset.length; i < length; ++i) {
       retVal += charset.charAt(Math.floor(Math.random() * n));
       
